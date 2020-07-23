9d3793af10412f40249f1e71e3e1338bcb26e274
pat-och <patrickmaige@hotmail.com>
2020-07-24 01:56:58 +0200

    first refactoring - calculate sequence after signal transmission
    use real sequence of cells

--- src/UseCase/EmitCellsSignals.php
+++ src/UseCase/EmitCellsSignals.php
@@ -9,6 +9,23 @@ class EmitCellsSignals
 
     public function __invoke(string $sequence): string
     {
-        return '0,0,0';
+
+        $initialCells = explode(',', $sequence);
+
+        $newCells = array(1,1,1);
+
+        if ($initialCells[2] === $initialCells[1]) {
+            $newCells[0] = 0;
+        }
+
+        if ($initialCells[0] === $initialCells[2]) {
+            $newCells[1] = 0;
+        }
+
+        if ($initialCells[1] === $initialCells[0]) {
+            $newCells[2] = 0;
+        }
+
+        return implode(',', $newCells);
     }
 }
