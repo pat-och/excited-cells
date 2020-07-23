ea54dbc1ab05324d26a4f323b594ad4cc4a4d551
pat-och <patrickmaige@hotmail.com>
2020-07-24 00:59:28 +0200

    supress useless controller
    for the moment :)

--- src/UseCase/EmitCellsSignals.php
+++ src/UseCase/EmitCellsSignals.php
@@ -7,13 +7,6 @@ namespace App\UseCase;
 class EmitCellsSignals
 {
 
-    /**
-     * EmitCellsSignals constructor.
-     */
-    public function __construct()
-    {
-    }
-
     public function __invoke()
     {
         // TODO: Implement __invoke() method.
