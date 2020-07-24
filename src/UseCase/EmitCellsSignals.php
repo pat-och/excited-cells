75454c09e691db8eb48c33c8ce8a56dd670ed035
pat-och <patrickmaige@hotmail.com>
2020-07-24 02:16:15 +0200

    refacto mutualise neighbor comparaison

--- src/UseCase/EmitCellsSignals.php
+++ src/UseCase/EmitCellsSignals.php
@@ -12,22 +12,30 @@ class EmitCellsSignals
 
         $initialCells = explode(',', $sequence);
 
-        $newCells = array(1,1,1);
+        $finalCells = array(1,1,1);
 
         foreach ($initialCells as $key => $value) {
-            if ($key === 0 && ($initialCells[2] === $initialCells[1])) {
-                $newCells[$key] = 0;
+
+            if ($key === 0) {
+                $previous = $initialCells[2];
+                $next = $initialCells[1];
             }
 
-            if ($key === 1 && ($initialCells[0] === $initialCells[2])) {
-                $newCells[$key] = 0;
+            if ($key === 1) {
+                $previous = $initialCells[0];
+                $next = $initialCells[2];
             }
 
-            if ($key === 2 && ($initialCells[1] === $initialCells[0])) {
-                $newCells[$key] = 0;
+            if ($key === 2) {
+                $previous = $initialCells[1];
+                $next = $initialCells[0];
+            }
+
+            if ($next === $previous) {
+                $finalCells[$key] = 0;
             }
         }
 
-        return implode(',', $newCells);
+        return implode(',', $finalCells);
     }
 }
