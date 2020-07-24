04cd3448109cf982ee9296cc88491016241ff73f
pat-och <patrickmaige@hotmail.com>
2020-07-24 03:42:53 +0200

    refactor with outline examples

--- tests/EmitSignalsOfCellsSequenceTest.php
+++ tests/EmitSignalsOfCellsSequenceTest.php
@@ -10,145 +10,30 @@ class EmitSignalsOfCellsSequenceTest extends TestCase
 
     /**
      * @test
-     * 0 0 0 -> 0 0 0
+     *
      */
-    public function OOOSequenceTransmissionShouldResultOnOOOSequence()
+    public function inputSequenceTransmissionShouldResultOnExpectedSequence()
     {
-        $EmitCellsSignals = new EmitCellsSignals();
+        $outline = [
+            '0,0,0' => '0,0,0',
+            '0,0,1' => '1,1,0',
+            '0,1,0' => '1,0,1',
+            '1,0,0' => '0,1,1',
+            '0,1,1' => '0,1,1',
+            '1,0,1' => '1,0,1',
+            '1,1,0' => '1,1,0',
+            '1,1,1' => '0,0,0',
+            '1,0,1,1' => '1,0,1,0',
+            '1,0,1,0' => '0,0,0,0',
+        ];
 
-        $this->assertEquals(
-            '0,0,0',
-            $EmitCellsSignals('0,0,0')
-        );
+        foreach ($outline as $input => $expected) {
+            $EmitCellsSignals = new EmitCellsSignals();
+
+            $this->assertEquals(
+                $expected,
+                $EmitCellsSignals($input)
+            );
+        }
     }
-
-    /**
-     * @test
-     * 0 0 1 -> 1 1 0
-     */
-    public function OOISequenceTransmissionShouldResultOnIIOSequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '1,1,0',
-            $EmitCellsSignals('0,0,1')
-        );
-    }
-
-    /**
-     * @test
-     * 0 1 0 -> 1 0 1
-     */
-    public function OIOSequenceTransmissionShouldResultOnIOISequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '1,0,1',
-            $EmitCellsSignals('0,1,0')
-        );
-    }
-
-    /**
-     * @test
-     * 1 0 0 -> 0 1 1
-     */
-    public function IOOSequenceTransmissionShouldResultOnOIISequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '0,1,1',
-            $EmitCellsSignals('1,0,0')
-        );
-    }
-
-    /**
-     * @test
-     * 0 1 1 -> 0 1 1
-     */
-    public function OIISequenceTransmissionShouldResultOnOIISequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '0,1,1',
-            $EmitCellsSignals('0,1,1')
-        );
-    }
-
-    /**
-     * @test
-     * 1 0 1 -> 1 0 1
-     */
-    public function IOISequenceTransmissionShouldResultOnIOISequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '1,0,1',
-            $EmitCellsSignals('1,0,1')
-        );
-    }
-
-    /**
-     * @test
-     * 1 1 0 -> 1 1 0
-     */
-    public function IIOSequenceTransmissionShouldResultOnIIOSequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '1,1,0',
-            $EmitCellsSignals('1,1,0')
-        );
-    }
-
-    /**
-     * @test
-     * 1 1 1 -> 0 0 0
-     */
-    public function IIISequenceTransmissionShouldResultOnOOOSequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '0,0,0',
-            $EmitCellsSignals('1,1,1')
-        );
-    }
-
-    /**
-     * @test
-     * 1 0 1 1 -> 1 0 1 0
-     */
-    public function IOIISequenceTransmissionShouldResultOnIOIOSequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '1,0,1,0',
-            $EmitCellsSignals('1,0,1,1')
-        );
-    }
-
-
-    /**
-     * @test
-     * 1 0 1 0  -> 0 0 0 0
-     */
-    public function IOIOSequenceTransmissionShouldResultOnOOOOSequence()
-    {
-        $EmitCellsSignals = new EmitCellsSignals();
-
-        $this->assertEquals(
-            '0,0,0,0',
-            $EmitCellsSignals('1,0,1,0')
-        );
-    }
-
-
-
 }
