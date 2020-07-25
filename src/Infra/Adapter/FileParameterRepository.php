85b07022fdf0855e0a53e08d829562cddf087337
pat-och <patrickmaige@hotmail.com>
2020-07-25 14:59:42 +0200

    add method stubs

--- src/Infra/Adapter/FileParameterRepository.php
+++ src/Infra/Adapter/FileParameterRepository.php
@@ -9,4 +9,13 @@ use App\SecondaryPort\ParameterRepositoryInterface;
 class FileParameterRepository implements ParameterRepositoryInterface
 {
 
+    public function getParameters(): array
+    {
+        // TODO: Implement getParameters() method.
+    }
+
+    public function addParameter(int $steps, string $sequence): void
+    {
+        // TODO: Implement addParameter() method.
+    }
 }
