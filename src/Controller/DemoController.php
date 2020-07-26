2efc8827aad402f0aeb0bf2447e5852bba7844e7
pat-och <patrickmaige@hotmail.com>
2020-07-26 20:39:02 +0200

    display results

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\SecondaryPort\ParameterRepositoryInterface;
+use App\UseCase\SendMultipleSignalPackets;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DemoController extends AbstractController
 {
     /**
-     * @Route("/demo", name="demo")
+     * @Route("/", name="demo")
      */
-    public function index()
+    public function index(ParameterRepositoryInterface $parameterRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DemoController.php',
+        $parameters = $parameterRepository->getParameters();
+        $results = array();
+        $sendMultipleSignalPackets = new SendMultipleSignalPackets();
+        foreach ($parameters as $parameter) {
+            $results[] = array(
+                'steps' => $parameter[0],
+                'sequence' => $parameter[1],
+                'result' => $sendMultipleSignalPackets($parameter[1], $parameter[0])
+            );
+        }
+
+        return $this->render('demo/index.html.twig', [
+            'controller_name' => 'DemoController',
+            'results' => $results
         ]);
     }
 }
