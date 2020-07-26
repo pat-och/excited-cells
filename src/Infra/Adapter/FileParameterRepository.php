c0659adb39e3a97377e3fdd7fc537ec3e78e88e7
pat-och <patrickmaige@hotmail.com>
2020-07-26 19:39:07 +0200

    implement infrastructure getParameters method

--- src/Infra/Adapter/FileParameterRepository.php
+++ src/Infra/Adapter/FileParameterRepository.php
@@ -9,13 +9,28 @@ use App\SecondaryPort\ParameterRepositoryInterface;
 class FileParameterRepository implements ParameterRepositoryInterface
 {
 
+    private array $parameters;
+
     public function getParameters(): array
     {
-        // TODO: Implement getParameters() method.
+        $content = file_get_contents('..\public\parameters\test.txt');
+
+        if (!$content) {
+            return $this->parameters;
+        }
+
+        $lines = explode('\r\n', $content);
+        foreach ($lines as $line) {
+            $items = explode(';', $line);
+            $this->parameters[] = array($items[0], $items[1]);
+        }
+
+        return $this->parameters;
+
     }
 
     public function addParameter(int $steps, string $sequence): void
     {
-        // TODO: Implement addParameter() method.
+        // TODO: Implement addParameter() method.$content
     }
 }
